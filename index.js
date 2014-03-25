fd76f08b1a69c175debb24b0865ff35e90391826
matst80 <mats.tornberg@gmail.com>
2014-03-25 21:30:52 +0100

    fungerande inloggning

--- index.js
+++ index.js
@@ -13,8 +13,26 @@ var FacebookStrategy = require('passport-facebook').Strategy;
 var FACEBOOK_APP_ID = '136862019768932';
 var FACEBOOK_APP_SECRET = '0946637710c380df58d81760e2a6f248';
 
-app.use(express.static('./public'));
-app.use(express.logger());
+
+app.configure(function() {
+  app.use(express.static('public'));
+  app.use(express.cookieParser());
+  app.use(express.bodyParser());
+  app.use(express.session({ secret: 'minsakranyckel' }));
+  app.use(passport.initialize());
+  app.use(passport.session());
+  app.use(app.router);
+});
+
+passport.serializeUser(function(user, done) {
+  console.log(user);
+  done(null, user);
+});
+
+passport.deserializeUser(function(user, done) {
+  console.log(user);
+  done(null, user);
+});
 
 app.get('/auth/facebook', passport.authenticate('facebook'));
 
@@ -26,6 +44,7 @@ app.get('/auth/facebook/callback',
   passport.authenticate('facebook', { successRedirect: '/index.htm',
                                       failureRedirect: '/login' }));
 
+
 passport.use(new FacebookStrategy({
     clientID: FACEBOOK_APP_ID,
     clientSecret: FACEBOOK_APP_SECRET,
@@ -34,12 +53,13 @@ passport.use(new FacebookStrategy({
   function(accessToken, refreshToken, profile, done) {
     
 	console.log(arguments);
-	done(null,{test:1});
+	done(null,{provider:'facebook',userid:profile.id});
   }
 ));
-
+/*
 app.use(function(req, res){
 	//console.log(req);
 	res.end(200,'hej');
 });
+*/
 app.listen(80);
