d0f79c2dda8c84f1d028a08ec807feb75a2eecf4
matst80 <mats.tornberg@gmail.com>
2014-03-26 21:42:13 +0100

    nytt

--- sqlhelper.js
+++ sqlhelper.js
@@ -6,9 +6,11 @@ module.exports = {
 		var update = [];
 		for(var k in obj)
 		{
-			keys.push(k);
-			values.push(obj[k]);
-			update.push(k+' = '+obj[k]);
+			if (k!="Id") {
+				keys.push(k);
+				values.push(obj[k]);
+				update.push(k+' = '+obj[k]);
+			}
 		}
 		return {keys:'`'+keys.join('`,`')+'`',values:"'"+values.join("','")+"'",update:update};
 	}
