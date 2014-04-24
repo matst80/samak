d4f09cb7af42927d20728c694068b4f1ce1cb82c
matst80 <mats.tornberg@gmail.com>
2014-04-24 21:36:16 +0200

    dfdfdfdf
    dfdf

--- restserver.js
+++ restserver.js
@@ -8,28 +8,33 @@ var restServer = function(basepath,app) {
 
 restServer.prototype.generateGet = function(obj)
 {
-	for(var k in obj)
+	
+	for(var i in obj)
 	{
-		var func = obj[k];
-		this.app.get(this.basepath+'/'+k,function(req,res) {
-			res.header("Content-Type", "text/javascript");
-			var url_parts = url.parse(req.url, true);
-    		var query = url_parts.query;
-    		query.user = req.user;
-			func(res,query,req);
-		});
+		
+		(function(func,t,k){
+			console.log(func,k);
+			t.app.get(t.basepath+'/'+k,function(req,res) {
+				res.header("Content-Type", "text/javascript");
+				var url_parts = url.parse(req.url, true);
+	    		var query = url_parts.query;
+	    		query.user = req.user;
+				func(res,query,req);
+			});
+		})(obj[i],this,i);
 	}
 }
 
 restServer.prototype.generatePost = function(obj)
 {
-	for(var k in obj)
+	for(var i in obj)
 	{
-		var func = obj[k];
-		this.app.post(this.basepath+'/'+k,function(req,res) {
-			res.header("Content-Type", "text/javascript");
-			func(res,req.body,req);
-		});
+		(function(func,t,k){
+			t.app.post(t.basepath+'/'+k,function(req,res) {
+				res.header("Content-Type", "text/javascript");
+				func(res,req.body,req);
+			});
+		})(obj[i],this,i);
 	}
 }
 
